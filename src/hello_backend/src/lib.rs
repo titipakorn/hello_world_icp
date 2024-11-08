0fb0a8a637ab2136d2bf6575d9d9950e27362b11
Titipakorn <titipakorn.p@gmail.com>
2024-11-09 00:29:53 +0700

    first commit

--- src/hello_backend/src/lib.rs
+++ src/hello_backend/src/lib.rs
@@ -1,4 +1,43 @@
-#[ic_cdk::query]
+use candid::{Principal};
+use ic_cdk::{query, update};
+use std::cell::RefCell;
+use std::collections::BTreeMap;
+
+type IdStore = BTreeMap<String, Vec<Principal>>;
+type ProfileStore = BTreeMap<Principal,Vec<String>>;
+
+thread_local! {
+static PROFILE_STORE: RefCell<ProfileStore> = RefCell::default();
+static ID_STORE: RefCell<IdStore> = RefCell::default();
+}
+
+#[query(name = "submitted_names")]
+fn submitted_names() -> Vec<String> {
+    let id = ic_cdk::api::caller();
+    let submitted_name = PROFILE_STORE.with(|profile_store| {
+    profile_store
+    .borrow()
+    .get(&id)
+    .cloned().unwrap_or_default()
+    });
+    submitted_name.iter().map(|name| format!("Hello, {}!", name)).collect::<Vec<String>>()
+}
+
+#[update]
 fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+let principal_id = ic_cdk::api::caller();
+ID_STORE.with(|id_store| {
+    let mut id_store = id_store.borrow_mut();
+    id_store.entry(name.clone())
+        .or_insert_with(Vec::new)
+        .push(principal_id);
+});
+
+PROFILE_STORE.with(|profile_store| {
+    let mut profile_store = profile_store.borrow_mut();
+    profile_store.entry(principal_id)
+        .or_insert_with(Vec::new)
+        .push(name.clone());
+});
+format!("Hello, {}!", name)
 }
--- src/hello_frontend/src/App.jsx
+++ src/hello_frontend/src/App.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react';
-import { hello_backend } from 'declarations/hello_backend';
+import { useState } from "react";
+import { hello_backend } from "declarations/hello_backend";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [greeting, setGreeting] = useState("");
+  const [greetingList, setGreetingList] = useState([]);
 
   function handleSubmit(event) {
     event.preventDefault();
     const name = event.target.elements.name.value;
-    hello_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
+    if (name !== "") {
+      hello_backend.greet(name).then((greeting_message) => {
+        setGreeting(greeting_message);
+      });
+    }
     return false;
   }
 
+  function getGreetingList() {
+    hello_backend.submitted_names().then((profile) => {
+      setGreetingList(profile);
+    });
+  }
+  console.log(greetingList);
   return (
     <main>
       <img src="/logo2.svg" alt="DFINITY logo" />
@@ -22,8 +31,23 @@ function App() {
         <label htmlFor="name">Enter your name: &nbsp;</label>
         <input id="name" alt="Name" type="text" />
         <button type="submit">Click Me!</button>
+        <button
+          type="button"
+          onClick={() => {
+            getGreetingList();
+          }}
+        >
+          ~Show Submitted Names~
+        </button>
       </form>
       <section id="greeting">{greeting}</section>
+      <div>
+        <ul>
+          {greetingList.map((greeting, index) => (
+            <li key={`li-${index}`}>{greeting}</li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
